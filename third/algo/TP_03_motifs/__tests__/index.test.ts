9cb7414962463b7785e8a3fcc93c16b7c71f8b00
CapitaineToinon <antoinesutter@gmail.com>
2022-04-14 02:17:29 +0200

    Moved patterns out of loop

--- third/algo/TP_03_motifs/__tests__/index.test.ts
+++ third/algo/TP_03_motifs/__tests__/index.test.ts
@@ -9,15 +9,15 @@ import { readFileSync } from 'fs'
 
 const MOCK_SOURCE = readFileSync(join(__dirname, 'mock.txt')).toString()
 
+const patterns = {
+  '1212': [2, 4, 6, 12, 98, 115, 199, 206, 208],
+  wwww: [17, 18, 19, 29, 163, 212],
+  bonsbonsbons: [34, 38, 42, 55, 182],
+  '1111abt1111ab': [68, 81, 102, 150],
+}
+
 ;[RabinKarp, Automates, KnuthMorrisPratt, BoyerMoore].map((Scanner) => {
   const scanner = new Scanner()
-
-  const patterns = {
-    '1212': [2, 4, 6, 12, 98, 115, 199, 206, 208],
-    wwww: [17, 18, 19, 29, 163, 212],
-    bonsbonsbons: [34, 38, 42, 55, 182],
-    '1111abt1111ab': [68, 81, 102, 150],
-  }
 
   for (const key in patterns) {
     test(`${scanner.name} ${key}`, async () => {
