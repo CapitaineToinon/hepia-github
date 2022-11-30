519d00f1a746f024de9dc02e1def132633dfc32a
CapitaineToinon <antoinesutter@gmail.com>
2022-11-30 19:03:17 +0100

    Working with n

--- fourth/distributedpython/server.py
+++ fourth/distributedpython/server.py
@@ -1,13 +1,34 @@
 import yaml
 import socket
 import argparse
+import pickle
 from threading import Thread, Lock
+from functools import reduce
 from time import sleep
 
 PORT: int = 65432
 
 
-class Config(object):
+class Message:
+    dist: int
+    nodes: list[str]
+
+    @staticmethod
+    def new(dist: int, nodes: list[str]) -> 'Message':
+        m = Message
+        m.dist = dist
+        m.nodes = nodes
+        return m
+
+    @staticmethod
+    def loads(bytes: bytes) -> 'Message':
+        return pickle.loads(bytes)
+
+    def dumps(self) -> str:
+        return pickle.dumps(self)
+
+
+class Config:
     id: int
     address: str
     neighbours: list['Config']
@@ -28,21 +49,25 @@ class Config(object):
                 self.neighbours.append(Config(neighbour))
 
     def is_neighbour(self, addr: tuple[str, int]) -> bool:
+        return False if self.get_neighbour_from_addr(addr=addr) is None else True
+
+    def get_neighbour_from_addr(self, addr: tuple[str, int]) -> 'Config':
         address, _ = addr
         for n in self.neighbours:
             if n.address == address:
-                return True
-        return False
+                return n
+        return None
 
 
 class Server(object):
     config: Config
     dist: dict[str, int] = {}
     first: dict[str, tuple[str, int]] = {}
-    nodes: int = 2  # TODO dynamic that
+    n: int = 3
 
     lock: Lock = Lock()
-    msg_count: int = 0
+    count: int = 0
+    messages: dict[str, dict] = {}
 
     @staticmethod
     def from_path(path: str) -> 'Server':
@@ -51,6 +76,8 @@ class Server(object):
 
     def __init__(self, config: Config) -> None:
         self.config = config
+        self.dist[self.config.address] = 0
+        self.first[self.config.address] = self.config.address
 
     def start(self) -> None:
         t = Thread(target=self.init_broadcast, name="init_broadcast")
@@ -70,38 +97,97 @@ class Server(object):
             except KeyboardInterrupt:
                 s.close()
 
+        t.join()
+
     def handler(self, conn: socket.socket, addr: tuple[str, int]) -> None:
         """
         Main handler that processes messages recieved on the socket
         """
         is_neighbour = self.config.is_neighbour(addr)
         who = 'neighbour' if is_neighbour else 'client'
-
         with conn:
-            print(f"Connected by {who} ({addr})")
+            address, _ = addr
+            print(f"Connected by {who} ({address})")
+            data = conn.recv(4096)  # TODO while True for more?
 
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                conn.sendall(data)
+            if is_neighbour:
+                neighbour = self.config.get_neighbour_from_addr(addr)
+                message = pickle.loads(data)
+                self.on_message(neighbour, message)
+            else:
+                print("TODO handle client messages")
 
-            with self.lock:
-                self.msg_count += 1
+    def on_message(self, neighbour: Config, message: dict):
+        print(f"Message from {neighbour.address} is {message}")
+        with self.lock:
+            self.count += 1
 
-                if self.msg_count == len(self.config.neighbours):
-                    print("receive message from all my neighbours!")
+            # save messages for later
+            self.messages[neighbour.address] = message
 
-    def init_broadcast(self, delay: int = 5):
-        print(f"broadcasting in {delay}s")
+            # got all messages for current t
+            if self.count == len(self.config.neighbours):
+                next_nodes: set[str] = set()
+
+                for (address, message) in self.messages.items():
+                    for node in message["nodes"]:
+                        # we found a better match!
+                        if node not in self.dist or self.dist[node] > message["dist"]:
+                            d = (message["dist"] + 1)
+                            current_d = self.dist[node] if node in self.dist else 'None'
+                            print(
+                                f"address {node} current is {current_d} but will now be {d}")
+                            self.dist[node] = d
+                            self.first[node] = address
+                            next_nodes.add(node)
+
+                # exit condition, we're done!
+                if message["dist"] >= self.n - 1:
+                    print("DONE")
+                    print("dist", self.dist)
+                    print("first", self.first)
+                    return
+
+                # otherwise keep going broadcast
+                next = {
+                    "dist": message["dist"] + 1,
+                    "nodes": next_nodes
+                }
+
+                print("NEXT", next)
+
+                if message["dist"] == 0:
+                    sleep(5)
+
+                self.broadcast_all(next)
+                self.count = 0
+                self.messages.clear()
+
+    def init_broadcast(self, delay: int = 10):
         sleep(delay)
-        print("broadcasting")
-        for neighbour in self.config.neighbours:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                print(f"sending to {neighbour.address}:{PORT}")
-                s.bind((self.config.address, 0))
-                s.connect((neighbour.address, PORT))
-                s.sendall(b"Hello, world")
+        self.broadcast_all({
+            "dist": 0,
+            "nodes": [self.config.address]
+        })
+
+    def broadcast_all(self, message: dict):
+        threads = [Thread(target=self.broadcast_to,
+                          name="broadcast_to",
+                          args=(neighbour, message,))
+                   for neighbour in self.config.neighbours]
+
+        for t in threads:
+            t.start()
+
+        for t in threads:
+            t.join()
+
+    def broadcast_to(self, to: Config,  message: dict):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.config.address, 0))
+            s.connect((to.address, PORT))
+            bytes = pickle.dumps(message)
+            s.send(bytes)
 
 
 if __name__ == "__main__":
